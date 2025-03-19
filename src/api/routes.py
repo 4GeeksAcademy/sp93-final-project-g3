c280eaa0a75e425ad72c70cebbc238f6ddf50742
Fernando Sartorius <92523759+Sartorius11@users.noreply.github.com>
2025-03-19 20:04:09 +0000

    nuevoscambios

--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,11 @@ def handle_hello():
     response_body = {}
     response_body['message'] = "Hello! I'm a message that came from the backend, check the network tab on the google inspector and you will see the GET request"
     return response_body, 200
+
+
+
+#https://cloudinary.com/
+#endpoint load image
+
+
+
