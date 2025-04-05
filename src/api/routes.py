1da6210c061bcb7480e7594543f33dfa0457afcc
Pedro Florit Docavo <pedroflorit@msn.com>
2025-04-05 11:52:29 +0000

    cloudinary baby

--- src/api/routes.py
+++ src/api/routes.py
@@ -659,5 +659,25 @@ def get_user_favorites():
     }), 200
 
     
-#https://cloudinary.com/
-#Endpoint load image
+@api.route('/user/photo', methods=['PUT'])
+@jwt_required()
+def update_profile_photo():
+    response_body = {}
+    data = request.json  # Recibe los datos JSON del frontend
+    user_id = get_jwt()['user_id']  # Obtiene el user_id del JWT
+    print("soy el data de upload photo", data)
+    print("upload photo user_id", user_id)
+    
+    # Obtén el usuario de la base de datos
+    row = Users.query.get(user_id)
+    if not row:
+        response_body['message'] = 'User not found'
+        return jsonify(response_body), 404
+    
+    # Actualiza la foto si existe en los datos
+    row.photo = data.get('photo', row.photo)
+    db.session.commit()
+    
+    response_body["message"] = "Photo uploaded successfully"
+    response_body["results"] = row.serialize()  # Asegúrate de que serialize devuelve la URL de la foto
+    return jsonify(response_body), 200
