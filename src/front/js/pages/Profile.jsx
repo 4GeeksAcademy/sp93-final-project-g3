ad6ab5399e81bb7c141c218bc070150027abdf9e
Pedro Florit Docavo <pedroflorit@msn.com>
2025-04-13 18:05:27 +0000

    seguimos

--- src/front/js/pages/Profile.jsx
+++ src/front/js/pages/Profile.jsx
@@ -70,6 +70,7 @@ export const Profile = () => {
               <i className="fas fa-edit"></i> Edit Profile
             </button>
           )}
+          
         </div>
         <nav className="profile-tabs">
           <div className="nav nav-tabs" id="profile-tabs" role="tablist">
